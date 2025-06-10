3f64e0f3ba30a4f6646015edd02064683ca56121
Arieslyvia <darieslyvia@gmail.com>
2025-06-10 03:12:05 -0700

    New Update(Movement)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,40 +10,31 @@ public class Movement : MonoBehaviour
     public Rigidbody bokTo;
     public float boktoSpeed;
     public float rotationSpeed;
-    Vector3 directionA = Vector3.forward;
-    float xDirection;
+    public Vector3 jump;
+    public float jumpF;
+   //bool isJumping;
 
     Vector3 startPos;
     Vector3 lastPos;
-    
+
 
 
     private void Start()
     {
         playeR = GetComponent<Animator>();
         bokTo = GetComponent<Rigidbody>();
-
+       
     }
 
     private void Update()
     {
-        bokTo.velocity = directionA * boktoSpeed;
-        float input = directionA.magnitude;
-
-        var lookDirection = directionA.normalized;
-        if ( lookDirection.magnitude >=0.1f)
-        {
-            var playerRotation = Quaternion.LookRotation( lookDirection );
-            transform.rotation = Quaternion.Slerp(transform.rotation, 
-                                playerRotation, rotationSpeed * Time.deltaTime);
-             
-        }
-        //bokTo.velocity = new Vector3(bokTo.velocity.x, bokTo.velocity.y, boktoSpeed);
+        transform.Translate(Vector3.forward * boktoSpeed);
         SwipeRL();
-       // JumpNSlide();
-        ShiftPosition();
 
+      
     }
+   
+    
 
     void SwipeRL()
     {
@@ -52,58 +43,70 @@ public class Movement : MonoBehaviour
         if (Input.GetMouseButtonDown(0)) // 0 represents the left mouseButton
         {
             startPos = Input.mousePosition;
-            
+
 
         }
         else if (Input.GetMouseButtonUp(0))
         {
             lastPos = Input.mousePosition;
-     
+            ShiftPosition();
         }
-        
+
     }
-       
+
 
     void ShiftPosition()
     {
-       var xDisplace = startPos.x - lastPos.x;
+        var xDisplace = startPos.x - lastPos.x;
         var yDisplace = startPos.y - lastPos.y;
 
         if (Mathf.Abs(xDisplace) > Mathf.Abs(yDisplace))
         {
             if (startPos.x - lastPos.x < 0)
             {
-                playeR.transform.eulerAngles = new Vector3(boktoSpeed, 90f, 0);
+                playeR.transform.eulerAngles = new Vector3(boktoSpeed, transform.eulerAngles.y+90f, 0);
                 //transform.Rotate(new Vector3(0f, 90f, 0f));
-                bokTo.velocity = new Vector3(boktoSpeed, bokTo.velocity.y, 0);
-
+                //bokTo.velocity = new Vector3(boktoSpeed, bokTo.velocity.y, 0);
 
                 Debug.Log("Swipe Right");
 
             }
             else
             {
-                playeR.transform.eulerAngles = new Vector3(boktoSpeed, -90f, 0);
-                bokTo.velocity = new Vector3(-boktoSpeed, bokTo.velocity.y, 0);
+                playeR.transform.eulerAngles = new Vector3(boktoSpeed, transform.eulerAngles.y+ -90f, 0);
+                //bokTo.velocity = new Vector3(-boktoSpeed, bokTo.velocity.y, 0);
                 Debug.Log("Swipe Left");
             }
         }
-        else 
+        else
         {
             if (startPos.y - lastPos.y < 0)
             {
                 Debug.Log("up");
-
+                bokTo.AddForce(jump, ForceMode.Impulse);
             }
             else
             {
                 Debug.Log("down");
+                
+
             }
         }
 
     }
-
     
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+            //bokTo.SetBool("Jump", false);
+
+        }
+
+    }
+*/
+
 }
 
 
