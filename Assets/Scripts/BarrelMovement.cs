cfeda2d1106670ab38f00330f6056dc7e4eab5e5
Arieslyvia <darieslyvia@gmail.com>
2025-06-25 01:58:57 -0700

    Final upload apk

--- Assets/Scripts/BarrelMovement.cs
+++ Assets/Scripts/BarrelMovement.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.Progress;
 
 public class BarrelMovement : MonoBehaviour
 {
